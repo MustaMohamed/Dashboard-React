377d15c788199be44fab61324774ce63a07f6501
MustaMohamed <loofey_123>
2018-12-30 14:42:40 +0200

    init project structure and packages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+
+// semantic css package
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
